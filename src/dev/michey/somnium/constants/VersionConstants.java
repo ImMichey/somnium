092886843abdbeac4f6a49a3b6daebd558342627
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 20:42:11 +0200

    Initial commit

---
+++ src/dev/michey/somnium/constants/VersionConstants.java
@@ -0,0 +1,9 @@
+package dev.michey.somnium.constants;
+
+public class VersionConstants {
+
+    public static final String SOMNIUM_NAME = "Somnium";
+    public static final String SOMNIUM_VERSION = "1.0.0";
+    public static final int SOMNIUM_BUILD = 1;
+
+}
---
+++ src/dev/michey/somnium/main/SomniumLauncher.java
@@ -0,0 +1,11 @@
+package dev.michey.somnium.main;
+
+import static dev.michey.somnium.constants.VersionConstants.*;
+
+public class SomniumLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Running " + SOMNIUM_NAME + " v" + SOMNIUM_VERSION + " (b" + SOMNIUM_BUILD + ")");
+    }
+
+}
